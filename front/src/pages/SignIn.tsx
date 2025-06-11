76f67091290ec18d34eaa971d555ea680e29bb5a
Aruzhan Nurgozha <aruzhan.nurgozha@nu.edu.kz>
2025-06-11 13:51:42 +0500

    change back

--- front/src/pages/SignIn.tsx
+++ front/src/pages/SignIn.tsx
@@ -14,13 +14,14 @@ const SignIn: React.FC = () => {
     setLoading(true);
     setError("");
     try {
-      const response = await fetch("http://127.0.0.1:8000/login", {
+      const response = await fetch("http://localhost:8000/login)", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ username, password }),
       });
+      console.log(response);
       if (!response.ok) {
         const data = await response.json();
         throw new Error(data.detail || "Login failed");
